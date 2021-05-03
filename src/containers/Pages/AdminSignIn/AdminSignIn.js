75b5c546003fdf60ba015e2d6be5fefde593d3bd
phongvanngo <19520834@gm.uit.edu.vn>
2021-05-03 11:15:03 +0700

    first commit

---
+++ src/containers/Pages/AdminSignIn/AdminSignIn.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AdminSignIn() {
+    return (
+        <div>
+            <h1>Admin login</h1>
+            <button>log in</button>
+        </div>
+    )
+}
