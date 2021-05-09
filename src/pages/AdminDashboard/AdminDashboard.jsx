1a54e32afa084fb8adf5781fb59132e1b380e43f
phongvanngo <19520834@gm.uit.edu.vn>
2021-05-09 07:51:42 +0700

    transition collapse hidden sidebar

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,21 +1,35 @@
 import { adminLogOut } from 'app/redux/adminAuthSlice';
 import Dropdown from 'pages/LandingPage/Dropdown';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AdminDashboardRoutes from './AdminDashboard.routes';
 import Navbar from './Navbar';
+import "./AdminDashboard.scss";
 
 export default function LandingPage() {
     const dispatch = useDispatch();
     const handleLogout = () => {
         dispatch(adminLogOut());
     }
+
+    const handleToggleSidebar = () => {
+        const adminLayout = document.getElementById("admin-dashboard-container");
+        const test = document.getElementById("demo");
+        test.classList.toggle("show");
+        adminLayout.classList.toggle("show-sidebar");
+    }
+
     return (
         <Router>
-            <div className="grid gap-5 grid-cols-12">
-                <aside className="col-span-2">left</aside>
-                <div className="col-span-10">right </div>
+            <div id="admin-dashboard-container" className="admin-dashboard-container show-sidebar">
+                <aside className="admin-sidebar">left</aside>
+                <div className="admin-main">
+                    <div>
+                        <button onClick={() => { handleToggleSidebar() }}>thu gon</button>
+                    </div>
+                    <div className="bg-red-500 w-10 h-5" id="demo">hi</div>
+                </div>
             </div>
         </Router>
     )
