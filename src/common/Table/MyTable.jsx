381804ef56025680c5f2a85906c14ac23bd76616
phongvanngo <19520834@gm.uit.edu.vn>
2021-05-13 14:25:44 +0700

    update ui

--- src/common/Table/MyTable.jsx
+++ src/common/Table/MyTable.jsx
@@ -7,24 +7,22 @@ export default function MyTable() {
     <div className="w-full">
       <div className="w-full ">
         <div className="w-full flex justify-between items-center mb-5">
-          <div className="flex items-center h-auto ">
+          <div className="flex items-center h-10 ">
             <input
-              className="appearance-none rounded-l-full border w-30 py-2 px-3 text-gray-700 leading-tight focus:outline-none "
+              className="h-full appearance-none rounded-l-full border w-30 py-2 px-3 text-gray-700 leading-tight focus:outline-none"
               placeholder="Tìm kiếm"
             />
-            <div className="bg-admin_color_1 rounded-r-full flex flex-col items-center h-full w-10 p-1">
-              <i className="bx bx-search-alt text-lg text-white"></i>
+            <div className="bg-admin_color_1 rounded-r-full h-full w-10 p-1">
+              <i className="bx bx-search-alt text-2xl text-white m-auto text-center"></i>
             </div>
           </div>
         </div>
         <div className="p-6 flex justify-between items-center h-20 border-b border-gray-200 rounded-t-3xl bg-white">
           <h1 className="text-xl font-medium">Danh sách hệ thống rạp</h1>
-          <div className="flex items-center">
-            <button className="flex items-center bg-admin_color_1 appearance-none  rounded-full w-100 h-full  py-2 px-8 text-admin_color_2 leading-tight hover:bg-indigo-900 focus:outline-none">
-              <i className="bx bx-plus mr-2 align-middle block"></i>
-              <span>Thêm rạp mới</span>
-            </button>
-          </div>
+          <button className="flex items-center bg-admin_color_1 appearance-none  rounded-full w-100 h-full  py-2 px-8 text-admin_color_2 leading-tight hover:bg-indigo-900 focus:outline-none">
+            <i className="bx bx-plus mr-2 align-middle block"></i>
+            <span>Thêm rạp mới</span>
+          </button>
         </div>
         <div className="px-8 py-6 rounded-b-3xl bg-white shadow-sm">
           <table className="table-auto w-full mb-10">
